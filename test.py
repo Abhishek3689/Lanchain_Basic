845e7f236d3ce6f30181b065095e6c1845889ca3
Abhishek3689 <abhisek.nishad3689@gmail.com>
2023-11-19 13:56:59 +0530

    update gitignore

--- test.py
+++ test.py
@@ -6,7 +6,11 @@ import streamlit as st
 from langchain.prompts import PromptTemplate
 from constants import openai_key
 
-os.environ["OPEN"]
+os.environ["OPENAI_API_KEY"]=openai_key 
 st.title("This is Home Page for LLM Open AI")
-st.text_input("Enter the topic you want to search")
+input_text=st.text_input("Enter the topic you want to search")
 
+llm=openai(temperature=.8)
+
+if input_text:
+    st.write(llm(input_text))
