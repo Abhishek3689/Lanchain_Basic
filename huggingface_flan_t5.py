c3822e338697cea96e910dc69fae4a6c476d47a0
Abhishek3689 <abhisek.nishad3689@gmail.com>
2023-12-07 22:03:50 +0530

    update huggig face/

---
+++ huggingface_flan_t5.py
@@ -0,0 +1,22 @@
+import os
+import streamlit as st
+import langchain
+from langchain import HuggingFaceHub
+from constants.py import HUGGINGFACEHUB_TOKEN
+
+os.environ['HUGGINGFACEHUB_API_TOKEN']=HUGGINGFACEHUB_TOKEN
+
+llm=HuggingFaceHub(repo_id='google/flan-t5-large',model_kwargs={'temperature':0.3,'max_length':64})
+
+
+
+st.title("This is Home page of Langchain")
+st.header("Welcome to Langchain")
+input=st.text_input("Please ask the question")
+res=llm(input)
+st.write(res)
+# if input=='abhi':
+#     st.write("Enterd value is 1")
+# else:
+#     st.write("Enter value is different")
+print("This is home page")
