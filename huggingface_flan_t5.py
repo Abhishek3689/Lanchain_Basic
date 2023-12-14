0b645c2cfa67bfaeeb841a454a806745a79ff237
Abhishek3689 <abhisek.nishad3689@gmail.com>
2023-12-14 22:19:23 +0530

    files updated

--- huggingface_flan_t5.py
+++ huggingface_flan_t5.py
@@ -1,10 +1,13 @@
 import os
+
 import streamlit as st
 import langchain
+from dotenv import load_dotenv
 from langchain import HuggingFaceHub
-from constants.py import HUGGINGFACEHUB_TOKEN
+#from constants.py import HUGGINGFACEHUB_TOKEN
 
-os.environ['HUGGINGFACEHUB_API_TOKEN']=HUGGINGFACEHUB_TOKEN
+load_dotenv()
+os.environ['HUGGINGFACEHUB_API_TOKEN']=os.getenv('HUGGINGFACEHUB_TOKEN')
 
 llm=HuggingFaceHub(repo_id='google/flan-t5-large',model_kwargs={'temperature':0.3,'max_length':64})
 
--- test.py
+++ test.py
@@ -2,15 +2,22 @@
 # import openai
 import os
 from langchain.llms import openai
+#from openai import OpenAI
+from dotenv import load_dotenv
 import streamlit as st
-from langchain.prompts import PromptTemplate
-from constants import openai_key
+#from langchain.prompts import PromptTemplate
 
-os.environ["OPENAI_API_KEY"]=openai_key 
+load_dotenv()
+
+os.environ["OPENAI_API_KEY"]=os.getenv('openai_key')
+llm=openai.OpenAI(temperature=.6)
+
+#llm=OpenAI(temperature=.6)
+
 st.title("This is Home Page for LLM Open AI")
 input_text=st.text_input("Enter the topic you want to search")
 
-llm=openai(temperature=.8)
+
 
 if input_text:
     st.write(llm(input_text))
